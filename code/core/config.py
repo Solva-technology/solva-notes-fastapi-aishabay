ef816f98a068a0bf13baa645986ae5a07fd2b68d
Aisha <aisha.zhumagul@alumni.nu.edu.kz>
2025-08-21 15:12:07 +0500

    update default values for env variables

--- code/core/config.py
+++ code/core/config.py
@@ -2,14 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    PRODUCTION: bool
-    APP_TITLE: str
-    DESCRIPTION: str
-    POSTGRES_USER: str
+    PRODUCTION: bool = False
+    APP_TITLE: str = "Notes App"
+    DESCRIPTION: str = "Application for notes"
+    POSTGRES_USER: str = "postgres"
     POSTGRES_PASSWORD: str
     DATABASE_URL: str
-    SECRET_WORD: str = "SECRET"
-    ADMIN_AUTH_SECRET_KEY: str = "SECRET"
+    SECRET_WORD: str
+    ADMIN_AUTH_SECRET_KEY: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
